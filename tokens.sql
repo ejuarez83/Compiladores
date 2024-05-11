71dc85e41d3c2b94b118cb37fcd8e6230e5bbe23
Erick Juarez <112781378+ejuarez83@users.noreply.github.com>
2024-05-11 04:12:58 -0600

    Create tokens.sql

---
+++ tokens.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `tokens` (
+  `token` char(50) DEFAULT NULL,
+  `lexema` varchar(1000) DEFAULT NULL,
+  `linea` int(4) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci 
